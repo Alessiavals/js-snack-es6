84709d2208c4120e149c021578d0882ae408bfbc
Alessiavals <alessiavalentini96@gmail.com>
2025-01-08 14:41:35 +0100

    1 snack

---
+++ main.js
@@ -0,0 +1,77 @@
+// SNACK 1 
+
+const automobili = [
+    { 
+        nome: 'Fiat',
+        modello: '500',
+        alimentazione: 'GPL'
+    },
+    {
+        nome: 'Audi',
+        modello: 'A3',
+        alimentazione: 'Benzina'
+    },
+    {
+        nome: 'Hyundai',
+        modello: 'i20',
+        alimentazione: 'GPL'
+    },
+    {
+        nome: 'Fiat',
+        modello: 'Punto',
+        alimentazione: 'Diesel'
+    },
+    {
+        nome: 'BMW',
+        modello: 'Serie 3',
+        alimentazione: 'Diesel'
+    },
+    {
+        nome: 'Fiat',
+        modello: 'Panda',
+        alimentazione: 'GPL'
+    },
+    {
+        nome: 'Volkswagen',
+        modello: 'Golf',
+        alimentazione: 'Diesel'
+    },
+    {
+        nome: 'Ford',
+        modello: 'Focus',
+        alimentazione: 'Metano'
+    },
+    {
+        nome: 'Tesla',
+        modello: 'Model 3',
+        alimentazione: 'Elettrico'
+    },
+    {
+        nome: 'Hyundai',
+        modello: 'Kona',
+        alimentazione: 'Elettrico'
+    }
+]
+
+console.log(automobili)
+
+const autoBenzina = [];
+const autoDiesel = [];
+const altreAuto = [];
+
+for (let index = 0; index < automobili.length; index++) {
+    const auto = automobili[index];
+
+    if (auto.alimentazione === 'Benzina') {
+        autoBenzina.push(auto)
+    } else if (auto.alimentazione === 'Diesel') {
+        autoDiesel.push(auto)
+    } else {
+        altreAuto.push(auto)
+    }
+    
+}
+
+console.log(autoBenzina)
+console.log(autoDiesel)
+console.log(altreAuto)
